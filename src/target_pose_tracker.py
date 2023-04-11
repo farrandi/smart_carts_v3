5cb164c4683dbcd20e3373b8ea0b53e9deec28a1
Iain Copland <iain.daoai@gmail.com>
2023-04-11 14:18:44 -0700

    changed value

--- src/target_pose_tracker.py
+++ src/target_pose_tracker.py
@@ -27,10 +27,10 @@ import sys
 import time
 
 ####### GLOBAL VARIABLES ########
-LOWER_RED1 = np.array([0,200,120])
+LOWER_RED1 = np.array([0,200,50])
 UPPER_RED1 = np.array([10,255,255])
 
-LOWER_RED2 = np.array([170,200,120])
+LOWER_RED2 = np.array([170,200,50])
 UPPER_RED2 = np.array([180,255,255])
 
 # LOWER_GREEN = np.array([40, 40, 40])
